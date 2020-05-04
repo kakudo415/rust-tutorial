d2ae6f1ecba7179cbc4a12e04725e89d22515231
Kakudo <kakudo415@gmail.com>
2020-05-04 15:14:48 +0900

    update slice

--- s4-3_slice/src/main.rs
+++ s4-3_slice/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hoge fuga foo bar");
+    let word = first_word(&s);
+    println!("{}", word);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
