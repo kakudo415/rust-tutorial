a7272ebca4884cb014022fcb9f47afc73b964eff
Kakudo <kakudo415@gmail.com>
2020-05-09 16:54:02 +0900

    +

---
+++ s6-2_match/src/main.rs
@@ -0,0 +1,31 @@
+#![allow(dead_code)] // 警告がうざったいので
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+fn value_in_cents(coin: Coin) -> i32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        }
+    }
+}
+
+fn main() {
+    let coin = Coin::Quarter(UsState::Alaska);
+    println!("Coin = {}", value_in_cents(coin));
+}
